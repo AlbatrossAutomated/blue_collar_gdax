5ac8036009b85d799ecc9485bc558000f5ee4226
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2018-03-11 21:06:57 -0600

    first commit

---
+++ spec/support/bot_settings.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class BotSettings
+  COVERAGE = 0.35
+  BUY_DOWN_INTERVAL = 0.2
+  PROFIT_INTERVAL = 0.2
+  PRINT_MANTRA = false
+  HOARD_QUOTE_PROFITS = true
+  BASE_CURRENCY_STASH = 0.0
+  ORDER_BACKFILLING = false
+  CANCEL_RETRIES = 10
+  RESERVE = 0.0
+end
